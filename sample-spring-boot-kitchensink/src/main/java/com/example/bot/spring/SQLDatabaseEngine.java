c41178596a394f868e491974efd925575be6af76
Edward Bradley <eabradley@connect.ust.hk>
2017-09-23 16:25:05 +0800

    Ready for Lab3 Testing

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -5,14 +5,45 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 
 @Slf4j
 public class SQLDatabaseEngine extends DatabaseEngine {
 	@Override
-	String search(String text) throws Exception {
+	String search(String text) throws Exception {	// text is the input text the user types in.
 		//Write your code here
-		return null;
+		String result = null;
+		String pg_getResponse = "SELECT response FROM chat_keyphrases WHERE keyphrase = '" + text + "'";
+		
+		try {
+			Connection postgres_connect = getConnection();
+			PreparedStatement pg_stmt = postgres_connect.prepareStatement(pg_getResponse);
+			ResultSet rs = pg_stmt.executeQuery();
+			
+			while (rs.next()) {
+				result = rs.getString("response");
+			}
+		} catch (SQLException e) {
+			log.info("IOException while reading from DB: {}", e.toString());
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pg_stmt != null)
+					pg_stmt.close();
+				if (postgres_connect != null)
+					postgres_connect.close();
+			} catch (IOException ex) {
+				log.info("IOException while closing from DB: {}", ex.toString());
+			}
+		}
+		
+		if (result != null)
+			return result;
+		throw new Exception("NOT FOUND");
 	}
 	
 	
